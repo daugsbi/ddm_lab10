c2b7a0750625e8f3d49fa836bb3997d75dacd011
Ralf Jeppesen <ralf.jeppesen@gmail.com>
2016-05-10 14:16:29 +0200

    Code kommentiert; Sale Analysis Output formatiert

---
+++ HadoopExercise/src/ch/fhnw/ddm/HadoopExercise/Dictionary/Dictionary.java
@@ -0,0 +1,81 @@
+package ch.fhnw.ddm.HadoopExercise.Dictionary;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+/**
+ * Main Klasse, welche ein ToolRunner initialisiert und ausführt 
+ * 
+ * @author Denis Augsburger, Tobias Giess, Ralf Jeppesen
+ */
+public class Dictionary extends Configured implements Tool {
+	
+	final static String[] inputFiles = { "res//French.txt", "res//German.txt", "res//Italian.txt" };
+	final static String outputPath = "res//dictionary_output";
+	
+	/**
+	 * Main Einstiegsfunktion
+	 * 
+	 * @param args Not used
+	 */
+	public static void main(String[] args) throws Exception {
+	    int res = ToolRunner.run(new Dictionary(), args);
+	    System.exit(res);
+	}
+	
+	/**
+	 * Hadoop Tool Runner, welcher ein Job konfiguriert und ausführt 
+	 * 
+	 * @param args Not used
+	 */
+	@Override
+	public int run(String[] args) throws Exception {
+		// Bestehende Konfiguration laden
+		Configuration conf = getConf();
+		
+		// Konfiguration so anpassen, dass als Delimter [Tab] verwendet wird
+		conf.set("mapreduce.input.keyvaluelinerecordreader.key.value.separator", "\t");
+
+		// Instanz laden
+		Job job = Job.getInstance(conf, "mapreducedictionary");
+		
+		// Ausführendes Jar, aufgrund der Klasse (diese Klasse), setzen 
+		job.setJarByClass(this.getClass());
+		
+		// InputFormater-Klasse der Map-Funktion setzen, welche den Output formatiert
+		job.setInputFormatClass(KeyValueTextInputFormat.class);
+		
+		// Eigene Mapper and Reducer Klasse setzen
+		job.setMapperClass(DictionaryMapper.class);
+		job.setReducerClass(DictionaryReducer.class);
+		
+		// OutputFormat, von Key und Value, der Reduce-Funktion setzen
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		
+		// Input Files angeben 
+		for (String file : inputFiles) {
+			FileInputFormat.addInputPath(job, new Path(file));	
+		}
+
+		// Output Ordner löschen
+		FileUtils.deleteDirectory(new File(outputPath)); 
+
+		// Output Path angeben 
+		FileOutputFormat.setOutputPath(job, new Path(outputPath));
+		
+	    // Job ausführen und Resultat abwarten
+	    return job.waitForCompletion(true) ? 0 : 1;
+	}
+}
---
+++ HadoopExercise/src/ch/fhnw/ddm/HadoopExercise/Dictionary/DictionaryReducer.java
@@ -0,0 +1,40 @@
+package ch.fhnw.ddm.HadoopExercise.Dictionary;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/**
+ * Reducer, welche die Übersetzungen (Deutsch, Französisch, Italienisch) zu einem Wort (Englisch), zusammenfügt
+ * 
+ * @author Denis Augsburger, Tobias Giess, Ralf Jeppesen
+ */
+public class DictionaryReducer extends Reducer<Text, Text, Text, Text> {
+	private Text translations = new Text();
+	
+	/**
+	 * Die eigentliche Reduce Funktion, welche die Zusammenfügung durchführt
+	 * 
+	 * @param key Wort (Englisch)
+	 * @param values Übersetzungen in den verschiedenen Sprachen (Deutsch, Französisch, Italienisch)
+	 * @param context Context in welcher der Reduce-Output geschrieben wird
+	 * @output Pro Wort (Englisch) eine Zeile mit allen Übersetzungen im Format "[Wort][Tab][Übersetzung1]|[Übersetzung2]|..."
+	 */
+	@Override
+	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        StringBuilder sb = new StringBuilder();
+		
+		for (Text val : values){
+			if (sb.length() > 0) {
+				sb.append("|");
+			}
+			
+			sb.append(val.toString());
+        }
+		
+		translations.set(sb.toString());
+		
+		context.write(key, translations);
+	}
+}
